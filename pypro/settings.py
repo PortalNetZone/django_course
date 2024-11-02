194a72891190d351080448a94c94bd096576b1ff
marcosranes <marcosranes@gmail.com>
2024-11-02 01:33:58 -0300

    create staticfiles directory

--- pypro/settings.py
+++ pypro/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = 'staticfiles/'
 # == aded 10.29.2024 ==
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 # == aded 10.31.2024 ==
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
