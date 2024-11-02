2e30ce0bc2e9157020732f1796b124c1218f0842
marcosranes <marcosranes@gmail.com>
2024-11-02 01:30:41 -0300

    create staticfiles directory

--- pypro/settings.py
+++ pypro/settings.py
@@ -117,11 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 # Configuração de arquivos estáticos
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 # == aded 10.29.2024 ==
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 # == aded 10.31.2024 ==
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
